cda33bfba33ae628dda98cd862d657418900690c
Student Q.. Public <student@nyos.org>
2026-04-16 11:17:49 -0500

    just testing how to add files to the commit list then commit it also my commit message is being written in Notepad for some reason lolz

---
+++ testFile.py
@@ -0,0 +1,9 @@
+print('i wonder if ill grow 10 thousand inches and be like oliver rioux')
+print('i also wonder if i can import stuff')
+
+import random
+
+print('i can!!!! thats tuffff')
+
+if(1==1):
+	print(True)
